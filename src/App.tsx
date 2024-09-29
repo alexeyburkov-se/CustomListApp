cba714ffe6bc3c05b3b4e53a31aca557e0d5c1d1
Alexey Burkov <alexeyburkovse@gmail.com>
2024-09-29 17:03:26 +0300

    Create simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
+import { ListPage } from "./pages/listPage";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  const mainRouter = createBrowserRouter([
+    {
+      path: "/list/item/:listId",
+      element: <ListPage />,
+    },
+    {
+      path: "/list/new",
+      element: <ListPage />,
+    },
+    {
+      path: "/",
+      loader: () => {
+        throw redirect("/list/new");
+      },
+    },
+  ]);
+  return <RouterProvider router={mainRouter} />;
+};
 
 export default App;
