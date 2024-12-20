6bddddeb0d289e8364e41c832726a03b2107b228
Alexey Burkov <alexeyburkovse@gmail.com>
2024-12-20 11:48:16 +0300

    Add internal structure types and support functions

--- src/internalData/creationFunctions.ts
+++ src/internalData/creationFunctions.ts
@@ -2,12 +2,12 @@ import { allListsDataKey, AllListsDataShapeV1 } from "./dataSchemas";
 
 export const createNewList = (): string => {
   const allListsData = JSON.parse(
-    localStorage.getItem(allListsDataKey) ?? '{"count":"0","lastId":"0"}'
+    localStorage.getItem(allListsDataKey()) ?? '{"count":"0","lastId":"0"}',
   ) as AllListsDataShapeV1;
-  allListsData.count += 1;
-  allListsData.lastId += 1;
-  const listId = `list_${allListsData.lastId}`;
+  allListsData.c += 1;
+  allListsData.l += 1;
+  const listId = `list_${allListsData.l}`;
   allListsData.listIds.push(listId);
-  localStorage.setItem(allListsDataKey, JSON.stringify(allListsData));
-  return listId
+  localStorage.setItem(allListsDataKey(), JSON.stringify(allListsData));
+  return listId;
 };
--- src/internalData/dataSchemas.ts
+++ src/internalData/dataSchemas.ts
@@ -1,7 +1,63 @@
-export const allListsDataKey = "___all";
+import { ListJsonSchemaV1 } from "../loaders/fileSchemas";
+
+export const allListsDataKey = () => "a";
+
+export const listDataKey = (listId: number) => `l${listId}`;
+
+export const listItemDataKey = (listKey: string, itemId: number) =>
+  `${listKey}i${itemId}`;
+
+export const listItemPropertyDataKey = (itemKey: string, propId: number) =>
+  `${itemKey}p${propId}`;
+
+export const filterItemPropertyKeysByItemKey = (itemKey: string, keys: string[]) =>
+  keys.filter((v) => v.includes(itemKey));
+
+export const filterItemKeysByListKey = (listKey: string, keys: string[]) =>
+  keys.filter((v) => v.includes(listKey));
 
 export interface AllListsDataShapeV1 {
-  count: number;
-  lastId: number;
-  listIds: string[];
+  v: "1";
+  c: number;
+  l: number;
 }
+
+export interface ListDataShapeV1 {
+  v: "1";
+  n: string;
+  c: number;
+  l: number;
+}
+
+export interface ListItemDataShapeV1 {
+  c: number;
+  l: number;
+}
+
+export const JsonSchemaItemTypeToInternalItemTypeConverter = {
+  text: 1,
+  label: 2,
+  title: 3,
+  rating: 4,
+  checkBox: 5,
+  ratingRatio: 6,
+} as const;
+
+export type ConvertItemJsonSchemaToInternalShapeV1<T> = T extends {
+  propName: string;
+  propType: infer U;
+  propValue: infer V;
+}
+  ? U extends ListJsonSchemaV1["main"][0]["properties"][0]["propType"]
+    ? {
+        n: string;
+        t: (typeof JsonSchemaItemTypeToInternalItemTypeConverter)[U];
+        v: V;
+      }
+    : never
+  : never;
+
+export type ListItemPropertyDataShapeV1 =
+  ConvertItemJsonSchemaToInternalShapeV1<
+    ListJsonSchemaV1["main"][0]["properties"][0]
+  >;
