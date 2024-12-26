cdc2b5a504d01074326f89c906652251d2cb2d02
Alexey Burkov <alexeyburkovse@gmail.com>
2024-12-27 01:10:43 +0300

    Remove redundant type

--- src/loaders/loaderV1/loader.ts
+++ src/loaders/loaderV1/loader.ts
@@ -1,11 +1,10 @@
-import { BaseListJsonSchema } from "../baseSchemas";
 import { ListLoadErrors, ListLoadResultType } from "../mainLoader";
 import { ZodValidationListV1 } from "./fileSchema";
 
-export const loadListV1 = (data: BaseListJsonSchema): ListLoadResultType => {
+export const loadListV1 = (data: unknown): ListLoadResultType => {
   const result = ZodValidationListV1.safeParse(data);
   if (!result.success) {
-    // todo: log errors
+    // todo log errors
     return { success: false, result: ListLoadErrors.InvalidFileSchema };
   }
   // todo processing
