27c1171c934a39d28287b7039d5cca1f3197c760
Alexey Burkov <alexeyburkovse@gmail.com>
2025-01-16 18:30:41 +0300

    Fix import from react router

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@mui/material";
 import { loadList } from "../loaders/mainLoader";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 export const HomePage = () => {
   const [isLoading, setLoading] = useState(false);
