56f49534c973f65659ae0f01b629fd3c326c3845
Alexey Burkov <rubicks_cube@mail.ru>
2025-02-02 19:47:58 +0300

    Add representation of list item

--- src/components/ListItemComponent.tsx
+++ src/components/ListItemComponent.tsx
@@ -1,18 +1,68 @@
 import { Control, useFieldArray } from "react-hook-form";
 import { ListType } from "../loaders/mainLoader";
+import {
+  Box,
+  Button,
+  Collapse,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { KeyboardArrowDown } from "@mui/icons-material";
+// todo add darkening of content
+// todo add hover animation for collapse
 
 export const ListItemComponent = ({
-  key,
   index,
+  data,
   control,
 }: {
-  key: string;
   index: number;
+  data: ListType["main"][0];
   control: Control<ListType>;
 }) => {
+  const [expanded, setExpanded] = useState(false);
   const { fields } = useFieldArray({
     name: `main.${index}.properties`,
     control,
   });
-  return <div key={key}></div>;
+  return (
+    <Paper elevation={3}>
+      <Collapse
+        in={expanded}
+        collapsedSize={"3em"}
+        onClick={() => !expanded && setExpanded(true)}
+      >
+        <Stack>
+          <Box sx={{display: "flex"}}>
+            <Typography sx={{flex: 1}}>Title</Typography>
+            <Box>
+              <Button onClick={() => expanded && setExpanded(false)}>
+                <KeyboardArrowDown
+                  sx={[
+                    {
+                      transition: "0.3s",
+                    },
+                    expanded
+                      ? {
+                          transform: "rotate(180deg)",
+                          opacity: 1,
+                        }
+                      : {
+                          transform: "rotate(0)",
+                          opacity: 0,
+                        },
+                  ]}
+                />
+              </Button>
+            </Box>
+          </Box>
+          {fields.map((field, index) => (
+            <div key={index}>prop {index}</div>
+          ))}
+        </Stack>
+      </Collapse>
+    </Paper>
+  );
 };
