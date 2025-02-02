f84c0dd76cfa7909b23bb16fd096b165b830a3d2
Alexey Burkov <rubicks_cube@mail.ru>
2025-02-02 19:47:21 +0300

    Add general settings and improve the look

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -2,7 +2,10 @@ import { Navigate } from "react-router";
 import { ListType } from "../loaders/mainLoader";
 import { useFieldArray, useForm } from "react-hook-form";
 import { ListItemComponent } from "../components/ListItemComponent";
+import { Button, Collapse, Container, Paper, Stack } from "@mui/material";
 import { useListData } from "../misc/listDataContext";
+import { useState } from "react";
+import { KeyboardArrowUp } from "@mui/icons-material";
 
 const ListPageInternal = ({ data }: { data: ListType }) => {
   const { control } = useForm<ListType>({
@@ -12,12 +15,44 @@ const ListPageInternal = ({ data }: { data: ListType }) => {
     name: "main",
     control,
   });
+
+  const [settingsOpen, setSettingsOpen] = useState(false);
   return (
-    <form>
-      {fields.map((field, index) => (
-        <ListItemComponent key={field.id} control={control} index={index} />
-      ))}
-    </form>
+    <Container>
+      <form>
+        <Stack spacing={3}>
+          <Paper elevation={1}>
+            <Stack>
+              <Button onClick={() => setSettingsOpen(!settingsOpen)}>
+                General settings
+                <KeyboardArrowUp
+                  sx={[
+                    { transition: "0.3s" }, // todo make equal transition for collapse and arrow
+                    settingsOpen
+                      ? {
+                          transform: "rotate(0)",
+                        }
+                      : {
+                          transform: "rotate(-180deg)",
+                          opacity: 0,
+                        },
+                  ]}
+                />
+              </Button>
+              <Collapse in={settingsOpen}>Some settings</Collapse>
+            </Stack>
+          </Paper>
+          {fields.map((field, index) => (
+            <ListItemComponent
+              key={field.id}
+              data={field}
+              control={control}
+              index={index}
+            />
+          ))}
+        </Stack>
+      </form>
+    </Container>
   );
 };
 
