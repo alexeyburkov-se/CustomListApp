d9910b630d2c35da94d64c702739b06e1b9dc4a1
Alexey Burkov <rubicks_cube@mail.ru>
2025-03-12 18:04:40 +0300

    Add new element button

--- src/components/ListGeneralComponent.tsx
+++ src/components/ListGeneralComponent.tsx
@@ -1,7 +1,10 @@
 import { Control, useFieldArray } from "react-hook-form";
 import { ListItemComponent } from "./ListItemComponent";
 import { ListType } from "../loaders/mainLoader";
+import { Box, IconButton, Stack } from "@mui/material";
 import { Dispatch, SetStateAction } from "react";
+import { Add } from "@mui/icons-material";
+import { useTranslation } from "react-i18next";
 
 export interface ListGeneralComponentProps {
   control: Control<ListType>;
@@ -11,19 +14,34 @@ export interface ListGeneralComponentProps {
 export const ListGeneralComponent = ({
   control,
 }: ListGeneralComponentProps) => {
-}: {
-  control: Control<ListType>;
-}) => {
-  const { fields } = useFieldArray({
+  const { fields, append } = useFieldArray({
     name: "main",
     control: control,
   });
+  const { t } = useTranslation();
 
   return (
     <Stack spacing={1}>
       {fields.map((field, index) => (
         <ListItemComponent key={field.id} control={control} itemIndex={index} />
       ))}
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <IconButton
+          onClick={() => {
+            append({
+              properties: [
+                {
+                  propertyName: "titleExample",
+                  propertyType: "title",
+                  propertyValue: t("listPage.addItem.title"),
+                },
+              ],
+            });
+          }}
+        >
+          <Add />
+        </IconButton>
+      </Box>
     </Stack>
   );
 };
