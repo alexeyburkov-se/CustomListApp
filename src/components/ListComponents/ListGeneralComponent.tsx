271913949b2a10173de48d4e03dabf6dd5525636
Alexey Burkov <rubicks_cube@mail.ru>
2025-06-17 00:56:28 +0300

    Add states for items and updated dirty condition

--- src/components/ListComponents/ListGeneralComponent.tsx
+++ src/components/ListComponents/ListGeneralComponent.tsx
@@ -2,7 +2,7 @@ import { useFieldArray, useForm } from "react-hook-form";
 import { ListItemComponent } from "./ListItemComponent";
 import { ListType } from "../../loaders/mainLoader";
 import { Box, Button, IconButton, Stack } from "@mui/material";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { Add } from "@mui/icons-material";
 import { useTranslation } from "react-i18next";
 import {
@@ -39,7 +39,17 @@ export const ListGeneralComponent = ({
     name: "data",
     control: control,
   });
+  const [itemsChangedStatus, updateItemsChangedStatus] = useState(
+    fields.map(() => false),
+  );
   const { t } = useTranslation();
+
+  const childrenItemsIsDirty = itemsChangedStatus.reduce(
+    (acc, val) => acc || val,
+    false,
+  );
+
+  const isDirty = formState.isDirty || childrenItemsIsDirty;
 
   const newItemData = {
     properties: [
@@ -67,16 +77,14 @@ export const ListGeneralComponent = ({
   const onAddNew = () => {
     append(newItemData);
     setUnsavedItemsStatus(true);
+    updateItemsChangedStatus(itemsChangedStatus.concat([false]));
   };
 
   return (
     <DndContext sensors={useSensors(sensorM, sensorT)}>
       <Stack spacing={1}>
         <Box>
-          <Button
-            sx={[formState.isDirty || { display: "none" }]}
-            onClick={onSave}
-          >
+          <Button sx={[childrenItemsIsDirty || { display: "none" }]} onClick={onSave}>
             {t("listPage.saveChanges")}
           </Button>
         </Box>
